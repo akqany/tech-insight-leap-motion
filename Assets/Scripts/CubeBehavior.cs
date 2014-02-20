f299e88febd70c79e35fb9eba31545120381c7ca
Kenny Lin <kenny.lin@akqa.com>
2014-02-20 14:58:40 -0500

    Added spinning to the cube

--- Assets/Scripts/CubeBehavior.cs
+++ Assets/Scripts/CubeBehavior.cs
@@ -6,5 +6,6 @@ public class CubeBehavior : MonoBehaviour {
 	public void setScale(float scale)
 	{
 		gameObject.transform.localScale += new Vector3(scale - 1, scale - 1, scale - 1);
+		gameObject.transform.localEulerAngles += new Vector3(0, scale, scale * 0.5f);
 	}
 }
